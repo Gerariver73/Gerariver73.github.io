9914e9e64518232258a2d550a0aa63e8db2220d0
Gerariver73 <126636955+Gerariver73@users.noreply.github.com>
2023-03-01 00:11:37 -0300

    Add files via upload

---
+++ Desarrollo WEB Front end/js/function.js
@@ -0,0 +1,25 @@
+document.getElementById('botonocultar').addEventListener('click', function()
+{
+
+    document.getElementById('FS').style.display='none';
+
+});
+
+document.getElementById('botonmostrar').addEventListener('click', function()
+{
+
+    document.getElementById('FS').style.display='block';
+
+});
+
+
+function aumentar() {
+  document.getElementById('F1').style.fontSize='18px';
+  };
+function disminuir() {
+    document.getElementById('F1').style.fontSize='12px';
+    };  
+function normalizar() {
+        document.getElementById('F1').style.fontSize='15px';
+        };  
+
